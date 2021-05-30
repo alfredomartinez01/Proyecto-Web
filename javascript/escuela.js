a8b16b5e40fa93f3b0cb6d9fb5d483fcc1b299a1
ReinaLeonel <82613093+ReinaLeonel@users.noreply.github.com>
2021-05-29 21:36:20 -0500

    FuncionMostrar_NombreEscuela

--- javascript/escuela.js
+++ javascript/escuela.js
@@ -1,1 +1,12 @@
-
+function mostrar(id) {
+    elemento = document.getElementById("lesc");
+    elemento1 = document.getElementById("iesc");
+    if (id == "otra") {
+        elemento.style.display = 'inline';
+        elemento1.style.display = 'inline';
+    }
+    else{
+        elemento.style.display = 'none';
+        elemento1.style.display = 'none';
+    }
+}
