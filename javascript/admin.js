cdb870236b66e5e460b2884ce53e274dba004708
alfredo <alfredomartinezruiz01@gmail.com>
2021-06-09 18:17:39 -0500

    Registro, búsqueda y eliminación terminados

--- javascript/admin.js
+++ javascript/admin.js
@@ -1,71 +1,246 @@
 function menu(number) {
-    limpiarContenido();
+    limpiarPaginas();
 
     switch (number) {
-            case 1: // Contenido para registrar alumno
-
+        case 1: // Contenido para registrar alumno
             // Mostrando el frame de registro
             var registro = document.getElementById("formulario");
             registro.style.display = "block";
+            document.getElementById("formulario_registro").src = "../formulario.html";
             break;
 
         case 2: // Contenido para buscar alumno
-
-            // Motrando el select para la búsqueda
+            // Mostrando y limpiando el select para la búsqueda
             var busqueda = document.getElementById("busqueda");
-            console.log(busqueda);
-            busqueda.style.display = "block"; 
+            busqueda.style.display = "block";
+            var selector = document.getElementById("tipo_busqueda");
+            selector.selectedIndex = 0;
+            break;
+
+        case 4: // Contenido para eliminar alumno
+            // Mostrando y limpiando el select para la búsqueda
+            var eliminacion = document.getElementById("eliminacion");
+            eliminacion.style.display = "block";
+            var selector = document.getElementById("tipo_eliminacion");
+            selector.selectedIndex = 0;
             break;
     }
 }
+
 /* Agrega el formulario para las búsquedas*/
-function selectorBusqueda(){ 
+function selectorBusqueda() {
     /* Limpiando las busquedas anteiores */
-    var busquedas = document.getElementsByClassName("formulario_busqueda");
-    
-    for(var i = 0; i< busquedas.length; i++){
-        busquedas[i].style.display = "none";
-    }
+    limpiarPaginas();
+    /* Recoloca la búsqueda */
+    var busqueda = document.getElementById("busqueda");
+    busqueda.style.display = "block";
 
     var selector = document.getElementById("tipo_busqueda");
     var seleccionado = selector.options[selector.selectedIndex].value;
-  
-    if(seleccionado == "boleta"){
-        /* Agregando sus elementos */  
-        var boleta = document.getElementById("bus_boleta");
+
+    if (seleccionado == "boleta") {
+        /* Agregando sus elementos */
+        var boleta = document.getElementById("form-boleta");
         boleta.style.display = "block";
 
-    } else if(seleccionado == "curp"){         
-        /* Agregando sus elementos */  
-        var boleta = document.getElementById("bus_curp");
+    } else if (seleccionado == "curp") {
+        /* Agregando sus elementos */
+        var boleta = document.getElementById("form-curp");
         boleta.style.display = "block";
 
-    } else if(seleccionado == "grupo"){
-        /* Agregando sus elementos */  
-        var boleta = document.getElementById("bus_grupo");
+    } else if (seleccionado == "grupo") {
+        /* Agregando sus elementos */
+        var boleta = document.getElementById("form-grupo");
         boleta.style.display = "block";
-        
-    } else if(seleccionado == "hora"){     
-        /* Agregando sus elementos */  
-        var boleta = document.getElementById("bus_hora");
-        boleta.style.display = "block";  
 
-    } else if(seleccionado == "todos"){
-        var formulario = document.getElementById("bus_todos");
-        formulario.submit();        
-    } 
+    } else if (seleccionado == "hora") {
+        /* Agregando sus elementos */
+        var boleta = document.getElementById("form-hora");
+        boleta.style.display = "block";
+
+    } else if (seleccionado == "todos") {
+        setInformation('todo');
+    }
 }
-function limpiarContenido(){
+
+/* Agrega el formulario para las eliminaciones*/
+function selectorEliminacion() {
+    /* Limpiando las busquedas anteiores */
+    limpiarPaginas();
+    /* Recoloca la búsqueda */
+    var busqueda = document.getElementById("eliminacion");
+    busqueda.style.display = "block";
+
+    var selector = document.getElementById("tipo_eliminacion");
+    var seleccionado = selector.options[selector.selectedIndex].value;
+
+    if (seleccionado == "boleta") {
+        /* Agregando sus elementos */
+        var boleta = document.getElementById("form-boleta");
+        boleta.style.display = "block";
+
+    } else if (seleccionado == "curp") {
+        /* Agregando sus elementos */
+        var boleta = document.getElementById("form-curp");
+        boleta.style.display = "block";
+
+    } else if (seleccionado == "grupo") {
+        /* Agregando sus elementos */
+        var boleta = document.getElementById("form-grupo");
+        boleta.style.display = "block";
+
+    } else if (seleccionado == "hora") {
+        /* Agregando sus elementos */
+        var boleta = document.getElementById("form-hora");
+        boleta.style.display = "block";
+
+    } else if (seleccionado == "todos") {
+        deleteInformation('todo');
+    }
+}
+
+/* Selecciona que funcion se va llamar en cuestion de en qué pestaña está el usuario */
+function selectorOperacion(tipo){
+    var bloque_busqueda = document.getElementById("busqueda");
+    if(bloque_busqueda.style.display != "none"){ // Si la busqueda está activa
+        setInformation(tipo);
+    }
+    else{ // Su la eliminación está activa
+        deleteInformation(tipo);
+    }
+}
+
+/* Agrega el contenido a la tabla después de hacer una búsqueda */
+function setInformation(tipo) {
+    /* Elimina contenido de la tabla */
+    var filas = document.getElementsByTagName("td");
+    while (filas.length > 0) {
+        for (var i = 0; i < filas.length; i++) {
+            filas[i].parentNode.removeChild(filas[i]);
+        }
+    }
+    /* Muestra la tabla */
+    var tabla = document.getElementById("tabla-alumnos");
+    tabla.style.display = "block";
+
+    /* Validacion de los datos */
+
+    /* Peticiones dependiendo del campo */
+    var alumnos = [];
+    switch (tipo) {
+        case "todo":
+            /* Espacio para la peticion */
+            console.log("hola");
+            break;
+    }
+
+    // Alumnos a mostrar
+    alumnos = [
+        {
+            "boleta": 2020654545,
+            "curp": "MARA011ASJDKJASDHJD",
+            "nombre": "Alfredo Rodrigo Lopez Rodriguez",
+            "fechaNac": "2001-11-16",
+            "telefono": 5532983339,
+            "correo": "alfredomartinezruiz01@gmail.com",
+            "grupo": "2CM14",
+            "hora": "13:00",
+            "escuela": "Centro de estudios científicos y tecnológicos No. 3",
+            "estado": "Ciudad de México"
+        },
+        {
+            "boleta": 2020654545,
+            "curp": "MARA011ASJDKJASDHJD",
+            "nombre": "Alfredo Rodrigo Lopez Rodriguez",
+            "fechaNac": "2001-11-16",
+            "telefono": 5532983339,
+            "correo": "alfredomartinezruiz01@gmail.com",
+            "grupo": "2CM14",
+            "hora": "13:00",
+            "escuela": "Centro de estudios científicos y tecnológicos No. 3",
+            "estado": "Ciudad de México"
+        }
+    ];
+
+
+
+    /* Inserción en la tabla */
+    for (let i = 1; i < 11; i++) {
+        var row = document.getElementById(`tr-${i}`); // Obtenemos cada fila
+        var cell;
+
+        for (let o = 0; o < alumnos.length; o++) {
+            var cell = document.createElement('td');
+            switch (i) {
+                case 1:
+                    cell.innerHTML = alumnos[o].boleta;
+                    break;
+                case 2:
+                    cell.innerHTML = alumnos[o].curp;
+                    break;
+                case 3:
+                    cell.innerHTML = alumnos[o].nombre;
+                    break;
+                case 4:
+                    cell.innerHTML = alumnos[o].fechaNac;
+                    break;
+                case 5:
+                    cell.innerHTML = alumnos[o].telefono;
+                    break;
+                case 6:
+                    cell.innerHTML = alumnos[o].correo;
+                    break;
+                case 7:
+                    cell.innerHTML = alumnos[o].grupo;
+                    break;
+                case 8:
+                    cell.innerHTML = alumnos[o].hora;
+                    break;
+                case 9:
+                    cell.innerHTML = alumnos[o].escuela;
+                    break;
+                case 10:
+                    cell.innerHTML = alumnos[o].estado;
+                    break;
+            }
+            row.appendChild(cell);
+        }
+    }
+}
+function deleteInformation(tipo){
+    /* Validacion de los datos */
+
+    /* Obtiene el número de registros */
+    var registros = 16;
+
+    /* Confirma la eliminación de los registros */
+    if(confirm(`Serán eliminados ${registros} registros`)){
+        // Se eliminan
+        alert(`${registros} registros fueron eliminados`);
+    } else{
+        alert("No se eliminó ningún registro");
+    }
+}
+
+/* Limpia en contenido de las pestañas */
+function limpiarPaginas() {
     /* Elimina registro */
     var registro = document.getElementById("formulario");
     registro.style.display = "none";
 
-    /* Elimina busqueda */
+    /* Elimina las opciones de búsqueda */
     var busqueda = document.getElementById("busqueda");
     busqueda.style.display = "none";
-
-    var busquedas = document.getElementsByClassName("formulario_busqueda");
-    for(var i = 0; i< busquedas.length; i++){
+    /* Elimina busqueda */
+    var busquedas = document.getElementsByClassName("datos_consulta");
+    for (var i = 0; i < busquedas.length; i++) {
         busquedas[i].style.display = "none";
     }
+    /* Elimina la tabla */
+    var tabla = document.getElementById("tabla-alumnos");
+    tabla.style.display = "none";
+
+    /* Elimina las opciones de eliminacion */
+    var busqueda = document.getElementById("eliminacion");
+    busqueda.style.display = "none";
 }
--- javascript/escuela.js
+++ javascript/escuela.js
@@ -5,7 +5,7 @@ function mostrar(id) {
         elemento.style.display = 'inline';
         elemento1.style.display = 'inline';
     }
-    else{
+    else {
         elemento.style.display = 'none';
         elemento1.style.display = 'none';
     }
@@ -13,60 +13,59 @@ function mostrar(id) {
 
 
 
-    
-    
-    
-   
-    
-    
-    
-    
 
-function confirmar_envio(){
-    var boleta =document.confirmar.nDeboleta.value;
+
+
+
+
+
+
+
+
+function confirmar_envio() {
+    var boleta = document.confirmar.nDeboleta.value;
     var nombre = document.confirmar.nombre.value;
-    var apaterno=document.confirmar.Apaterno.value;
-    var amaterno=document.confirmar.Amaterno.value;
-    var Fnac=document.confirmar.Fnacimiento.value;
-    var genero=document.confirmar.gen.value;
-    var curp=document.confirmar.CURP.value;
-    var calle=document.confirmar.calle.value;
-    var num=document.confirmar.numero.value;
-    var colonia=document.confirmar.colonia.value;
-    var cp=document.confirmar.codigo.value;
-    var tel=document.confirmar.tel.value;
-    var email=document.confirmar.correo.value;
-    var Escuela=document.confirmar.EscuelaProcedencia.value;
-    var otra=document.confirmar.NombreEscuela.value;
-    var estado=document.confirmar.estado.value;
-    var promedio=document.confirmar.Promedio.value;
-    var opcion=document.confirmar.Opcion.value;
+    var apaterno = document.confirmar.Apaterno.value;
+    var amaterno = document.confirmar.Amaterno.value;
+    var Fnac = document.confirmar.Fnacimiento.value;
+    var genero = document.confirmar.gen.value;
+    var curp = document.confirmar.CURP.value;
+    var calle = document.confirmar.calle.value;
+    var num = document.confirmar.numero.value;
+    var colonia = document.confirmar.colonia.value;
+    var cp = document.confirmar.codigo.value;
+    var tel = document.confirmar.tel.value;
+    var email = document.confirmar.correo.value;
+    var Escuela = document.confirmar.EscuelaProcedencia.value;
+    var otra = document.confirmar.NombreEscuela.value;
+    var estado = document.confirmar.estado.value;
+    var promedio = document.confirmar.Promedio.value;
+    var opcion = document.confirmar.Opcion.value;
 
-    if(confirm("Hola"+" "+ nombre + " "+ apaterno + " "+ amaterno + " "+
-     "verifica que los datos que ingresaste sean correctos:" +"\n"+
-    "Nombre: "+ nombre + "\n"+
-    "Apellido paterno: "+ apaterno + "\n"+
-    "Apellido materno: "+ amaterno + "\n"+
-    "Número de boleta: " + boleta + "\n"+
-    "Fecha de nacimiento:"+ Fnac + "\n"+
-    "Género: " + genero + "\n"+
-    "CURP: " + curp + "\n"+
-    "contacto:\n "+
-    "Calle: " + calle + "\n"+
-    "Número: " +num + "\n"+
-    "Colonia: " + colonia + "\n"+
-    "Código postal: " +cp + "\n"+
-    "Télefono/celular: " +tel + "\n"+
-    "E-mail: " +email + "\n"+
-    "Prodecencia: " +Escuela + "\n"+ 
-    "Procedencia: " + otra + "\n"+
-    "Estado de la República: " + estado + "\n"+
-    "Promedio: " + promedio + "\n"+
-    "Opción: " +opcion + "\n")){
-       // document.confirmar.submit();
-       return true;
-    }else{
-       return false;
+    if (confirm("Hola" + " " + nombre + " " + apaterno + " " + amaterno + " " +
+        "verifica que los datos que ingresaste sean correctos:" + "\n" +
+        "Nombre: " + nombre + "\n" +
+        "Apellido paterno: " + apaterno + "\n" +
+        "Apellido materno: " + amaterno + "\n" +
+        "Número de boleta: " + boleta + "\n" +
+        "Fecha de nacimiento:" + Fnac + "\n" +
+        "Género: " + genero + "\n" +
+        "CURP: " + curp + "\n" +
+        "contacto:\n " +
+        "Calle: " + calle + "\n" +
+        "Número: " + num + "\n" +
+        "Colonia: " + colonia + "\n" +
+        "Código postal: " + cp + "\n" +
+        "Télefono/celular: " + tel + "\n" +
+        "E-mail: " + email + "\n" +
+        "Prodecencia: " + Escuela + "\n" +
+        "Procedencia: " + otra + "\n" +
+        "Estado de la República: " + estado + "\n" +
+        "Promedio: " + promedio + "\n" +
+        "Opción: " + opcion + "\n")) {
+        // document.confirmar.submit();
+        return true;
+    } else {
+        return false;
     }
-        }
-        
+}
